b6a8afa02e546940238ae73f70404539ee3a150a
Eduard Teller <eduard@aieesti.ee>
2025-06-18 18:18:30 +0300

    initial commit | backend projekt initsialiseeritud spring initializr abil | frontend vite (react-ts) abil

---
+++ prooviylesanne-backend/src/main/java/com/rik/prooviylesanne/ProoviylesanneApplication.java
@@ -0,0 +1,13 @@
+package com.rik.prooviylesanne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProoviylesanneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProoviylesanneApplication.class, args);
+	}
+
+}
---
+++ prooviylesanne-backend/src/test/java/com/rik/prooviylesanne/ProoviylesanneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rik.prooviylesanne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProoviylesanneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
